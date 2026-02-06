5923c07f8bd2203693f0923d73671ded3ec117a2
Abhishek <abhisekchaudhary32@gmail.com>
2026-02-06 22:21:00 +0545

    portfolio

---
+++ My portfolio/script.js
@@ -0,0 +1,8 @@
+function sayHello() {
+  alert("Hello! Thanks for visiting my portfolio 😊");
+}
+
+function submitForm(event) {
+  event.preventDefault();
+  alert("Message sent successfully 🚀");
+}
